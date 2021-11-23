8f808f7d4e509d3241671233cca5591403ac8bb2
ImInnocent <ssje2006@naver.com>
2021-11-23 14:47:54 +0900

    Coding Convention + Change file name

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import User from '../types/User';
 import { useLocalStorage } from './LocalStorage';
 
--- src/hooks/useString.tsx
+++ src/hooks/useString.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 /**
  * string 타입의 state를 안정적으로 관리
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
  * @param {string | null} init 초기 데이터
  * @returns {[string, (any) => void]} 데이터와 set함수 반환
  */
-export default function useString(init?: string): [string, (value: any) => void] {
+export default (init?: string): [string, (value: any) => void] => {
   const [str, setStr] = useState<string>(init === undefined ? '' : init);
 
   /**
