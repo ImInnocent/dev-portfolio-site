adc0e8a83ed660458c6f1c6b35943c5c05820a58
ImInnocent <ssje2006@naver.com>
2021-12-02 20:08:10 +0900

    Fix filename 2

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import useString from '../hooks/StringState';
+import useString from '../hooks/useString';
 import { useAuth } from '../contexts/Auth';
 
 export default function SignupPage() {
